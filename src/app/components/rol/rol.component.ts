7bd576c617718a6e7934476db5c8732011e7abd2
Juan Pablo Pachar Viñan <jpablopachar1993@gmail.com>
2019-06-15 10:33:56 -0500

    Metodo guardar rol

--- src/app/components/rol/rol.component.ts
+++ src/app/components/rol/rol.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { RolService } from 'src/app/services/rol.service';
+import { UserService } from 'src/app/services/user.service';
+import { Rol } from 'src/app/models/rol';
 
 @Component({
   selector: 'app-rol',
   templateUrl: './rol.component.html',
-  styleUrls: ['./rol.component.css']
+  styleUrls: ['./rol.component.css'],
+  providers: [RolService, UserService]
 })
 export class RolComponent implements OnInit {
+  public token;
+  public opcionBoton: string;
+  public estado: string;
 
-  constructor() { }
+  constructor(private userService: UserService, private rolService: RolService) {
+    this.token = this.userService.obtenerToken();
+  }
 
   ngOnInit() {
+    console.log('¡Componente rol cargado!');
+    this.listarRoles();
+  }
+
+  listarRoles() {
+    this.rolService.listarRoles(this.token).subscribe(res => this.rolService.roles = res.roles as Rol[], error => console.log(<any>error));
   }
 
 }
